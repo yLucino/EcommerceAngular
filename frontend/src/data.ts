c6190a1c96294378d25ed146077f0f8595e6c923
yLucino <{ID}+{username}@users.noreply.github.com>
2024-06-08 20:00:58 -0300

    Add Header and Search visual

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -3,21 +3,66 @@ import { product } from './app/components/shared/models/product';
 export const sample_products: product[] = [
   {
     id: '1',
-    name: 'T-shirt Preta',
-    price: 180,
+    name: 'T-shirt Oversized Black Cotton',
+    price: 111.99,
     favorite: true,
     origins: ['São Paulo', 'Brazil'],
     imageUrl: '',
-    tags: ['T-shirt', 'Preta']
+    tags: ['T-shirt', 'Oversized', 'Black']
   },
   {
     id: '2',
-    name: 'Jaqueta Preta',
-    price: 400,
+    name: 'T-shirt Oversized Grateful White',
+    price: 139.99,
     favorite: true,
     origins: ['São Paulo', 'Brazil'],
     imageUrl: '',
-    tags: ['Jaqueta', 'Preta']
+    tags: ['T-shirt', 'Oversized', 'White']
   },
-
+  {
+    id: '3',
+    name: 'T-shirt Oversized Black Distorted',
+    price: 139.99,
+    favorite: true,
+    origins: ['São Paulo', 'Brazil'],
+    imageUrl: '',
+    tags: ['T-shirt', 'Oversized', 'Black']
+  },
+  {
+    id: '4',
+    name: 'T-shirt Oversized Roll Dice',
+    price: 139.99,
+    favorite: true,
+    origins: ['São Paulo', 'Brazil'],
+    imageUrl: '',
+    tags: ['T-shirt', 'Oversized', 'Black']
+  },
+  {
+    id: '5',
+    name: 'T-shirt Oversized Black Distorted',
+    price: 139.99,
+    favorite: true,
+    origins: ['São Paulo', 'Brazil'],
+    imageUrl: '',
+    tags: ['T-shirt', 'Oversized', 'Black']
+  },
+  {
+    id: '6',
+    name: 'Pants Elast Fit Flex Motion',
+    price: 75.90,
+    favorite: true,
+    origins: ['São Paulo', 'Brazil'],
+    imageUrl: '',
+    tags: ['Pants', 'Fit', 'Black']
+  },
+  {
+    id: '7',
+    name: 'Jacket Black College',
+    price: 319.99,
+    favorite: true,
+    origins: ['São Paulo', 'Brazil'],
+    imageUrl: '',
+    tags: ['Jacket', 'College', 'Black']
+  },
+  
 ]
